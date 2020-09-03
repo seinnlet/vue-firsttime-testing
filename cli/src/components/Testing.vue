a5032134c011cf287b8d89c5f74648cfba726c85
seinnletlethninn <sllhninn.6@gmail.com>
2020-09-03 09:23:58 +0630

    added cli vue

---
+++ cli/src/components/Testing.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h1>{{ title }}</h1>
+		<p>{{ msg }}</p>
+		<p>This is testing for child components.</p>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		props: {
+			msg: String
+		},
+		data() {
+			return {
+				title: 'Testing Vue'
+			}
+		}
+	};
+</script>
+
+<style type="text/css">
+	
+</style>
